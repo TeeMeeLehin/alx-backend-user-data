8cadee1785e3bf7cfdbbe19100b58e335e792b83
TeeMeeLehin <samemerald8@gmail.com>
2024-02-15 17:57:36 +0100

    basic auth setup

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """basic authentication module"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -13,4 +14,17 @@ class BasicAuth(Auth):
             if isinstance(authorization_header, str):
                 if authorization_header[:6] == "Basic ":
                     return authorization_header.split(' ')[1]
+        return None
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """decoding basic auth header func"""
+        if base64_authorization_header:
+            if isinstance(base64_authorization_header, str):
+                try:
+                    sub = base64.b64decode(base64_authorization_header)
+                    return sub.decode('utf-8')
+                except ValueError:
+                    return None
         return None
